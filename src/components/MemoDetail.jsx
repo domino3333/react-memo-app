33fe0facd324c4596f0d06de53dd04d53ae21109
domino3333 <busster@naver.com>
2026-02-10 11:21:24 +0900

    메모 디테일 수정, 메모 아이템에서 content가 한 줄을 넘어갈 시 ...으로 표현하도록 수정

--- src/components/MemoDetail.jsx
+++ src/components/MemoDetail.jsx
@@ -1,16 +1,25 @@
+import '../css/MemoDetail.css'
 
+const MemoDetail = ({id, content, date}) => {
+  return (
+    <div className="memo-detail">
+      {/* 상단 헤더 */}
+      <div className="memo-detail-header">
+        <h1 className="memo-detail-id">#{id}</h1>
+        <button className="memo-edit-btn" type="button">수정</button>
+      </div>
 
+      {/* 날짜 */}
+      <div className="memo-detail-date">{date}</div>
 
-const MemoDetail = ({id,content,date}) => {
-  return (<>
-    <h1>{id}</h1>
-    <button type="button">수정</button>
-    <p>{date}</p>
-    <p>{content}</p>
-    <button type="button">삭제</button>
-  </>)
+      {/* 내용 */}
+      <div className="memo-detail-content">{content}</div>
 
+      {/* 삭제 버튼 */}
+      <button className="memo-delete-btn" type="button">삭제</button>
+    </div>
+  )
 }
 
-
 export default MemoDetail
+
